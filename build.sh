0c6f3430c6243539f101d8b0323d40c4b378bcab
rollo <rollo.nike@gmail.com>
2017-04-24 17:34:56 -0700

    Update build.sh
    changed image tag

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION="1.0.0.4.${BUILD_NUMBER:-99}"
-IMAGE=gnschenker/node-sample
+IMAGE=rollo/node-sample
 ID=$(docker build  -t ${IMAGE}  .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
 
 docker tag ${ID} ${IMAGE}:${VERSION}
